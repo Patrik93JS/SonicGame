e070005077e3dc573e2d958ed8d890c1e7406527
Patrik <patrikludvik@gmail.com>
2025-06-15 11:31:57 +0200

    rat moving

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -1,14 +1,19 @@
-import type { FC } from 'react';
+import type { FC, ReactNode } from 'react';
 import { Beams } from './components/Beams';
 import { Floor } from './components/Floor';
 import { Roof } from './components/Roof';
 
-export const Layout: FC = () => {
+type Props = {
+  children: ReactNode;
+};
+
+export const Layout: FC<Props> = ({ children }) => {
   return (
     <div className="min-h-screen bg-red-200 flex items-center justify-center">
       <Floor />
       <Beams />
       <Roof />
+      {children}
     </div>
   );
 };
