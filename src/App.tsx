e05e91be2dfbc82ccf4cd6feb520825f7da8a903
Patrik <patrikludvik@gmail.com>
2025-06-15 12:22:50 +0200

    fix rat

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,36 @@
-import type { FC } from 'react';
+import { useState, type FC, type SetStateAction } from 'react';
 import { Layout } from './Layout';
 import { Rat } from './components/Rat';
 import { Bomb } from './components/Bomb';
 
 const App: FC = () => {
+  const [ratPosition, setRatPosition] = useState({ x: 50, y: 80 });
+  const [bombPosition, setBombPosition] = useState({ x: 50, y: 20 });
+  const colided = ratPosition.y === bombPosition.y;
+
+  console.log('ratPosition', ratPosition);
+  console.log('bombPosition', bombPosition);
+
+  const handleRatPosition = (
+    value: SetStateAction<{ x: number; y: number }>
+  ) => {
+    setRatPosition(value);
+  };
+
+  const handleBombPosition = (
+    value: SetStateAction<{ x: number; y: number }>
+  ) => {
+    setBombPosition(value);
+  };
+
   return (
     <Layout>
-      <Rat />
-      <Bomb />
+      <Rat ratPosition={ratPosition} handleRatPosition={handleRatPosition} />
+      <Bomb
+        colided={colided}
+        bombPosition={bombPosition}
+        handleBombPosition={handleBombPosition}
+      />
     </Layout>
   );
 };
--- src/components/Bomb.tsx
+++ src/components/Bomb.tsx
@@ -1,28 +1,40 @@
-import { useRef, useState, type FC } from 'react';
+import { useRef, type Dispatch, type FC, type SetStateAction } from 'react';
 import bombImg from './../assets/bombImg.png';
+import explosionImg from './../assets/explosionImg.png';
 
-type Props = {};
+type Props = {
+  colided: boolean;
+  bombPosition: { x: number; y: number };
+  handleBombPosition: Dispatch<SetStateAction<{ x: number; y: number }>>;
+};
 
-export const Bomb: FC<Props> = () => {
-  const [bombPosition, setBombPosition] = useState({ x: 50, y: 20 });
+export const Bomb: FC<Props> = ({
+  colided,
+  bombPosition,
+  handleBombPosition,
+}) => {
   const intervalRef = useRef<NodeJS.Timeout | null>(null);
 
-  console.log(bombPosition);
+  const generateRandomPosition = () => {
+    const x = Math.random() * (95 - 4) + 4;
+    handleBombPosition({ x, y: 20 });
+  };
 
   const startMoving = () => {
+    if (bombPosition.y === 80) reset();
+
     if (intervalRef.current === null) {
       intervalRef.current = setInterval(() => {
-        setBombPosition((prev) => {
+        handleBombPosition((prev) => {
           const step = 1;
           return { x: prev.x, y: Math.max(prev.y + step, 4) };
         });
-      }, 100);
+      }, 30);
     }
-    if (bombPosition.y === 80) reset();
   };
 
   const reset = () => {
-    setBombPosition({ x: 50, y: 20 });
+    generateRandomPosition();
     if (intervalRef.current !== null) {
       clearInterval(intervalRef.current);
       intervalRef.current = null;
@@ -32,11 +44,22 @@ export const Bomb: FC<Props> = () => {
   startMoving();
 
   return (
-    <img
-      src={bombImg}
-      alt="Bomb"
-      style={{ left: `${bombPosition.x}%`, top: `${bombPosition.y}%` }}
-      className="absolute bottom-[10%] transform -translate-x-1/2 w-16 h-16"
-    />
+    <>
+      {colided ? (
+        <img
+          src={explosionImg}
+          alt="Explosion"
+          style={{ left: `${bombPosition.x}%`, top: `${bombPosition.y}%` }}
+          className="absolute bottom-[10%] w-38 h-38"
+        />
+      ) : (
+        <img
+          src={bombImg}
+          alt="Bomb"
+          style={{ left: `${bombPosition.x}%`, top: `${bombPosition.y}%` }}
+          className="absolute transform -translate-x-1/2 w-16 h-16"
+        />
+      )}
+    </>
   );
 };
